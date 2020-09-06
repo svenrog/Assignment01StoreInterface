065730b786b4c6b376219151699cdb9f9226bf5b
Svenrog <sven.erik@geta.no>
2020-09-06 11:36:50 +0200

    Added project files.

---
+++ src/Assignment01StoreInterface/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Assignment01StoreInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hans-Johnny har precis startat en butik på nätet där han säljer film och musik till en smalare publik.
+
+            //Butiken har två adresser, en för fakturering och en besöksadress.
+            //Butikens utbud består av 25 unika filmer och unika 25 musikalbum.
+
+            //Resultatet skall presentera följande information.
+
+            //För filmer: Namn, Regissör, Snittbetyg från användare(0 - 10), Releasedatum, Speltid(i minuter och timmar), Pris.
+            //För musikalbum: Namn, Artist, Snittbetyg från användare(0 - 10), Releasedatum, Speltid(i minuter och timmar), Antal låtar, Pris.
+
+            //Album innehåller även en lista med låtar som skall innehålla följande information: Låtnamn, Speltid, Ytterligare artister(featuring)
+
+            //Följande skall skrivas ut:
+
+            //Butikens information(inklusive adresser).
+            //Filmutbud, sorterat efter releasedatum, fallande(nyast överst).
+            //Albumutbud, sorterat efter snittbetyg från användare, fallande(högst överst).
+
+            //Datamodellen skall i största möjliga mån dela information där det är möjligt.
+
+            //(Fiktiv data accepteras självklart)
+        }
+    }
+}
